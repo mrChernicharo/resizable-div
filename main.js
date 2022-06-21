56d43d30a16dcedefd3dcc307669045dc5747b23
Felipe Chernicharo <string7dev@gmail.com>
2022-06-21 00:07:08 -0400

    looking good

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 import './style.css';
 
 const app = document.querySelector('#app');
+const section = document.querySelector('#section');
 const thumb = document.querySelector('.thumb');
 const resizable = document.querySelector('.resizable');
-let { left, top, width, height } = resizable.getBoundingClientRect();
+let { top, height } = resizable.getBoundingClientRect();
+let { height: sectionHeight } = section.getBoundingClientRect();
 let isResizing = false;
 let isDragging = false;
-
-let dragPos = null;
 
 resizable.addEventListener('pointerdown', e => {
 	console.log('resizable:pointerdown', e);
 
 	isDragging = true;
-	dragPos = e.clientY;
+	document.body.style.cursor = 'move';
+});
+
+resizable.addEventListener('pointerover', e => {
+	if (!isResizing && !isDragging) {
+		document.body.style.cursor = 'grab';
+	}
+});
+
+resizable.addEventListener('pointerout', e => {
+	if (!isResizing && !isDragging) {
+		document.body.style.cursor = 'default';
+	}
 });
 
 thumb.addEventListener('pointerdown', e => {
@@ -25,65 +37,34 @@ thumb.addEventListener('pointerdown', e => {
 	document.body.style.cursor = 'row-resize';
 });
 
-thumb.addEventListener('pointerout', e => {
-	if (!isResizing) {
-		document.body.style.cursor = 'default';
-	}
-});
-
-thumb.addEventListener('pointerover', e => {
-	if (!isResizing) {
-		document.body.style.cursor = 'grab';
-	}
-});
-
 document.addEventListener('pointerup', e => {
 	console.log('document:pointerup', e);
 
 	isResizing = false;
 	isDragging = false;
-	dragPos = null;
 	document.body.style.cursor = 'default';
 });
 
 document.addEventListener('pointermove', e => {
-	if (isResizing) {
-		let _h = getComputedStyle(document.documentElement).getPropertyValue(
-			'--h'
-		);
-		let currentH = parseInt(_h);
-		let diff = e.clientY - top - currentH;
+	// resizing
+	if (
+		isResizing &&
+		height + e.movementY > 40 &&
+		top + height + e.movementY < sectionHeight
+	) {
+		height += e.movementY;
 
-		document.documentElement.style.setProperty(
-			'--h',
-			currentH + diff + 'px'
-		);
+		document.documentElement.style.setProperty('--h', height + 'px');
 	}
 
-	if (isDragging) {
-		let _top = parseInt(
-			getComputedStyle(document.documentElement).getPropertyValue(
-				'--top-offset'
-			)
-		);
-
-		// let offset = e.clientY - _top;
-		let dragDiff = e.clientY - _top;
-
-		top += dragDiff;
-
-		console.log({ dragPos, _top, dragDiff });
+	// dragging
+	if (
+		isDragging &&
+		top + e.movementY >= 0 &&
+		top + e.movementY <= sectionHeight - height
+	) {
+		top += e.movementY;
 
 		document.documentElement.style.setProperty('--top-offset', top + 'px');
 	}
 });
-
-// function onResize(entries) {
-// 	console.log(entries[0].contentRect);
-
-// 	// const { } = entries[0].contentRect
-// }
-
-// const resizeObserver = new ResizeObserver(onResize);
-
-// resizeObserver.observe(resizable);

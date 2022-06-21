c7db009a50d7e0770ae2c4f8f981219e5c81e86b
Felipe Chernicharo <string7dev@gmail.com>
2022-06-20 23:41:14 -0400

    moving forward

--- main.js
+++ main.js
@@ -7,25 +7,42 @@ let { left, top, width, height } = resizable.getBoundingClientRect();
 let isResizing = false;
 let isDragging = false;
 
+let dragPos = null;
+
+resizable.addEventListener('pointerdown', e => {
+	console.log('resizable:pointerdown', e);
+
+	isDragging = true;
+	dragPos = e.clientY;
+});
+
 thumb.addEventListener('pointerdown', e => {
-	console.log('pointerdown', e);
+	e.stopPropagation();
+	console.log('thumb:pointerdown', e);
+
 	isResizing = true;
-	// app.style.cursor = 'row-resize';
+
 	document.body.style.cursor = 'row-resize';
 });
 
 thumb.addEventListener('pointerout', e => {
-	if (!isResizing) document.body.style.cursor = 'default';
+	if (!isResizing) {
+		document.body.style.cursor = 'default';
+	}
 });
 
 thumb.addEventListener('pointerover', e => {
-	document.body.style.cursor = 'row-resize';
+	if (!isResizing) {
+		document.body.style.cursor = 'grab';
+	}
 });
 
 document.addEventListener('pointerup', e => {
-	console.log('pointerup', e);
+	console.log('document:pointerup', e);
 
 	isResizing = false;
+	isDragging = false;
+	dragPos = null;
 	document.body.style.cursor = 'default';
 });
 
@@ -42,4 +59,31 @@ document.addEventListener('pointermove', e => {
 			currentH + diff + 'px'
 		);
 	}
+
+	if (isDragging) {
+		let _top = parseInt(
+			getComputedStyle(document.documentElement).getPropertyValue(
+				'--top-offset'
+			)
+		);
+
+		// let offset = e.clientY - _top;
+		let dragDiff = e.clientY - _top;
+
+		top += dragDiff;
+
+		console.log({ dragPos, _top, dragDiff });
+
+		document.documentElement.style.setProperty('--top-offset', top + 'px');
+	}
 });
+
+// function onResize(entries) {
+// 	console.log(entries[0].contentRect);
+
+// 	// const { } = entries[0].contentRect
+// }
+
+// const resizeObserver = new ResizeObserver(onResize);
+
+// resizeObserver.observe(resizable);
